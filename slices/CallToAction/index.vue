14f9baaa35f579f8e5d8f1676960b4ca7e04ee19
Raphael Reichrudel <reichrudel.raphael@gmail.com>
2023-11-18 03:30:27 +0200

    hero horizontal and cta slices bugfixes

--- slices/CallToAction/index.vue
+++ slices/CallToAction/index.vue
@@ -9,33 +9,31 @@ defineProps(
     "index",
     "slices",
     "context",
-  ]),
+  ])
 );
 </script>
-
-<!-- This shit does NOT work properly on localhost:3000 lol -->
 
 <template>
   <Bounded
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    <div class="max-w-4xl mx-auto md:px-12 px-4 py-12 grid place-items-center rounded-lg bg-gradient-to-tr from-cyan-50 via-white to-emerald-50 shadow-xl">
-      <PrismicRichText
-        :field="slice.primary.heading"
-        class="mb-2 heading heading--sm"
+    <div class="max-w-4xl m-auto shadow-xl md:px-12 px-4 py-12 grid place-items-center rounded-lg bg-gradient-to-tr from-cyan-50 via-white to-emerald-50">
+      <PrismicText
         wrapper="h2"
+        class="heading heading--sm !font-semibold mb-4"
+        :field="slice.primary.heading"
       />
       <PrismicRichText
         :field="slice.primary.body"
         class="text-center text-slate-600 mb-8"
       />
-      <PrismicLink
-      :field="slice.primary.button_link"
+      <NuxtLink
+        :field="slice.primary.button_link"
         class="button"
       >
         {{ slice.primary.button_text }}
-      </PrismicLink>
+      </NuxtLink>
     </div>
   </Bounded>
 </template>
--- slices/Hero/index.vue
+++ slices/Hero/index.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { type Content } from "@prismicio/client";
-  // The array passed to `getSliceComponentProps` is purely optional.
-  // Consider it as a visual hint for you when templating your slice.
-  defineProps(
-    getSliceComponentProps<Content.HeroSlice>([
-      "slice",
-      "index",
-      "slices",
-      "context",
-    ]),
-  );
-</script>
 
-<!-- Horizontal shit doesn't really work for the image but who cares LOL! -->
+// The array passed to `getSliceComponentProps` is purely optional.
+// Consider it as a visual hint for you when templating your slice.
+defineProps(
+  getSliceComponentProps<Content.HeroSlice>([
+    "slice",
+    "index",
+    "slices",
+    "context",
+  ])
+);
+</script>
 
 <template>
   <Bounded
@@ -22,21 +21,34 @@ import { type Content } from "@prismicio/client";
     <div
       class="grid grid-cols-1 place-items-center"
       :class="{
-        'md:grid-cols-2': slice.variation == 'horizontal'
+        'md:grid-cols-2': slice.variation === 'horizontal'
       }"
     >
       <div
         class="flex flex-col"
         :class="{
-          'items-center text-center': slice.variation == 'default'
+          'items-center text-center': slice.variation === 'default'
         }"
       >
-        <PrismicRichText :field="slice.primary.heading" class="heading heading--xl mb-4 md:mb-8"/>
-        <PrismicRichText :field="slice.primary.body" class="mb-4 md:mb-8 text-2xl font-normal leading-10 font-body max-w-lg text-slate-600"/>
-        <PrismicLink :field="slice.primary.button_link" class="button mb-8 md:mb-10">
-          {{ slice.primary.button_text }}</PrismicLink>
-        <PrismicImage :field="slice.primary.image" class="max-w-4xl w-full drop-shadow-xl" />
+        <PrismicRichText
+          class="heading heading--xl md:mb-8 mb-4"
+          :field="slice.primary.heading"
+        />
+        <PrismicRichText
+          class="text-2xl font-normal leading-10 font-body text-slate-600 max-w-md mb-4 md:mb-8"
+          :field="slice.primary.body"
+        />
+        <NuxtLink
+          :field="slice.primary.button_link"
+          class="button mb-8 md:mb-10"
+        >
+          {{ slice.primary.button_text }}
+        </NuxtLink>
       </div>
+      <PrismicImage
+        :field="slice.primary.image"
+        class="drop-shadow-xl max-w-4xl w-full"
+      />
     </div>
   </Bounded>
 </template>
